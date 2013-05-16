5d55ab705d8e06535fe6c5a54200d4d72a3df33f
Simon Kornblith <simon@simonster.com>
2013-05-16 16:29:30 -0400

    Add tests

---
+++ test/test.jl
@@ -0,0 +1,165 @@
+using Memoize, Base.Test
+
+run = 0
+@memoize function simple(a)
+	global run += 1
+	a
+end
+@test simple(5) == 5
+@test run == 1
+@test simple(5) == 5
+@test run == 1
+@test simple(6) == 6
+@test run == 2
+@test simple(6) == 6
+@test run == 2
+
+run = 0
+@memoize function typed(a::Int)
+	global run += 1
+	a
+end
+@test typed(5) == 5
+@test run == 1
+@test typed(5) == 5
+@test run == 1
+@test typed(6) == 6
+@test run == 2
+@test typed(6) == 6
+@test run == 2
+
+run = 0
+@memoize function default(a=2)
+	global run += 1
+	a
+end
+@test default() == 2
+@test run == 1
+@test default() == 2
+@test run == 1
+@test default(2) == 2
+@test run == 1
+@test default(6) == 6
+@test run == 2
+@test default(6) == 6
+@test run == 2
+
+run = 0
+@memoize function default_typed(a::Int=2)
+	global run += 1
+	a
+end
+@test default_typed() == 2
+@test run == 1
+@test default_typed() == 2
+@test run == 1
+@test default_typed(2) == 2
+@test run == 1
+@test default_typed(6) == 6
+@test run == 2
+@test default_typed(6) == 6
+@test run == 2
+
+run = 0
+@memoize function kw(; a=2)
+	global run += 1
+	a
+end
+@test kw() == 2
+@test run == 1
+@test kw() == 2
+@test run == 1
+@test kw(a=2) == 2
+@test run == 1
+@test kw(a=6) == 6
+@test run == 2
+@test kw(a=6) == 6
+@test run == 2
+
+run = 0
+@memoize function kw_typed(; a::Int=2)
+	global run += 1
+	a
+end
+@test kw_typed() == 2
+@test run == 1
+@test kw_typed() == 2
+@test run == 1
+@test kw_typed(a=2) == 2
+@test run == 1
+@test kw_typed(a=6) == 6
+@test run == 2
+@test kw_typed(a=6) == 6
+@test run == 2
+
+run = 0
+@memoize function default_kw(a=1; b=2)
+	global run += 1
+	(a, b)
+end
+@test default_kw() == (1, 2)
+@test run == 1
+@test default_kw() == (1, 2)
+@test run == 1
+@test default_kw(1, b=2) == (1, 2)
+@test run == 1
+@test default_kw(1, b=3) == (1, 3)
+@test run == 2
+@test default_kw(1, b=3) == (1, 3)
+@test run == 2
+@test default_kw(2, b=3) == (2, 3)
+@test run == 3
+@test default_kw(2, b=3) == (2, 3)
+@test run == 3
+
+run = 0
+@memoize function default_kw_typed(a::Int=1; b::Int=2)
+	global run += 1
+	(a, b)
+end
+@test default_kw_typed() == (1, 2)
+@test run == 1
+@test default_kw_typed() == (1, 2)
+@test run == 1
+@test default_kw_typed(1, b=2) == (1, 2)
+@test run == 1
+@test default_kw_typed(1, b=3) == (1, 3)
+@test run == 2
+@test default_kw_typed(1, b=3) == (1, 3)
+@test run == 2
+@test default_kw_typed(2, b=3) == (2, 3)
+@test run == 3
+@test default_kw_typed(2, b=3) == (2, 3)
+@test run == 3
+
+run = 0
+@memoize function ellipsis(a, b...)
+	global run += 1
+	(a, b)
+end
+@test ellipsis(1) == (1, ())
+@test run == 1
+@test ellipsis(1) == (1, ())
+@test run == 1
+@test ellipsis(1, 2, 3) == (1, (2, 3))
+@test run == 2
+@test ellipsis(1, 2, 3) == (1, (2, 3))
+@test run == 2
+
+run = 0
+@memoize function kw_ellipsis(;a...)
+	global run += 1
+	a
+end
+@test kw_ellipsis() == {}
+@test run == 1
+@test kw_ellipsis() == {}
+@test run == 1
+@test kw_ellipsis(a=1) == {(:a, 1)}
+@test run == 2
+@test kw_ellipsis(a=1) == {(:a, 1)}
+@test run == 2
+@test kw_ellipsis(a=1, b=2) == {(:a, 1), (:b, 2)}
+@test run == 3
+@test kw_ellipsis(a=1, b=2) == {(:a, 1), (:b, 2)}
+@test run == 3
