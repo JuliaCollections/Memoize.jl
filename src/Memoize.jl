89b9e56af0897b74ed7dba0cc3f6c81a925f55e5
nkottary <kottarynishanth54@gmail.com>
2016-06-28 16:20:50 -0400

    Fix depwarns for v0.5

--- src/Memoize.jl
+++ src/Memoize.jl
@@ -1,5 +1,6 @@
 module Memoize
 export @memoize
+using Compat
 
 macro memoize(args...)
     if length(args) == 1
@@ -11,12 +12,12 @@ macro memoize(args...)
         error("Memoize accepts at most two arguments")
     end
 
-    if !isa(ex,Expr) || (ex.head != :function && ex.head != symbol("=")) ||
+    if !isa(ex,Expr) || (ex.head != :function && ex.head != @compat(Symbol("="))) ||
        isempty(ex.args) || ex.args[1].head != :call || isempty(ex.args[1].args)
         error("@memoize must be applied to a method definition")
     end
     f = ex.args[1].args[1]
-    ex.args[1].args[1] = u = symbol(string(f,"_unmemoized"))
+    ex.args[1].args[1] = u = @compat(Symbol(string(f,"_unmemoized")))
 
     args = ex.args[1].args[2:end]
 
